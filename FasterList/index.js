3d0d1f25b00616b23cbb3c5d9cae74ce7dbf3ca3
buhairong <22858111@qq.com>
2019-05-21 11:56:57 +0800

    add

--- FasterList/index.js
+++ FasterList/index.js
@@ -6,10 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import FlatListDemo from './pages/FlatListDemo';
 import {name as appName} from './app.json';
-import {StackNavigator} from 'react-navigation'
+import {createAppContainer, createStackNavigator} from 'react-navigation'
 
 
-const AppRoot = StackNavigator({
+const AppRoot = createStackNavigator({
     App: {
         screen: App
     },
@@ -21,4 +21,6 @@ const AppRoot = StackNavigator({
     }
 })
 
-AppRegistry.registerComponent(appName, () => AppRoot);
+const AppStackNavigatorContainer = createAppContainer(AppRoot)
+
+AppRegistry.registerComponent(appName, () => AppStackNavigatorContainer);
--- FasterList/pages/FlatListDemo.js
+++ FasterList/pages/FlatListDemo.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList, RefreshControl, ActivityIndicator} from 'react-native';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,13 +17,88 @@ const instructions = Platform.select({
 });
 
 type Props = {};
+
+const CITY_NAMES = ['北京', '上海', '广州', '深圳', '杭州', '苏州', '成都', '武汉', '郑州', '洛阳', '厦门', '青岛', '拉萨']
+
 export default class FlatListDemo extends Component<Props> {
+    constructor (props) {
+        super(props)
+        this.state = {
+            isLoading: false,
+            dataArray: CITY_NAMES
+        }
+    }
+
+    _renderItem (data) {
+        return <View style={styles.item}>
+            <Text style={styles.text}>
+                {data.item}
+            </Text>
+        </View>
+    }
+
+    loadData (refreshing) {
+        if (refreshing) {
+            this.setState({
+                isLoading: true
+            })
+        }
+
+        setTimeout(() => {
+            let dataArray = []
+
+            if (refreshing) {
+                for (let i = this.state.dataArray.length -1; i >= 0; i--) {
+                    dataArray.push(this.state.dataArray[i])
+                }
+            } else {
+                dataArray = this.state.dataArray.concat(CITY_NAMES)
+            }
+
+            this.setState({
+                dataArray: dataArray,
+                isLoading: false
+            })
+        }, 2000)
+    }
+
+    genIndicator () {
+        return <View style = {styles.indicatorContainer}>
+            <ActivityIndicator
+                style = {styles.indicator}
+                size = {'large'}
+                color = {'red'}
+                animating = {true}
+            />
+            <Text>正在加载更多...</Text>
+        </View>
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
-                <Text style={styles.instructions}>To get started, edit App.js</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
+                <FlatList
+                    data = {this.state.dataArray}
+                    renderItem = {(data) => this._renderItem(data)}
+                    /*refreshing = {this.state.isLoading}
+                    onRefresh = {() => {
+                        this.loadData()
+                    }}*/
+                    refreshControl = {
+                        <RefreshControl
+                            title = {'Loading'}
+                            colors = {['red']}
+                            tintColor = {'orange'}
+                            titleColor = {'red'}
+                            refreshing = {this.state.isLoading}
+                            onRefresh = {() => {
+                                this.loadData(true)
+                            }}
+                        />
+                    }
+                    ListFooterComponent = {() => this.genIndicator()}
+                    onEndReached = {() => {this.loadData()}}
+                />
             </View>
         );
     }
@@ -31,19 +106,26 @@ export default class FlatListDemo extends Component<Props> {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        flex: 1
+    },
+    item: {
+        backgroundColor: '#169',
+        height: 200,
+        marginRight: 15,
+        marginLeft: 15,
+        marginBottom: 15,
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        justifyContent: 'center'
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    text: {
+        color: 'white',
+        fontSize: 20
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+    indicatorContainer: {
+        alignItems: 'center'
     },
+    indicator: {
+        color: 'red',
+        margin: 10
+    }
 });
